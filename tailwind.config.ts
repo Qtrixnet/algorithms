9b069cdb55ca9035c724f6cc2d92b3bf4146a168
Kirill <qtrixnet@yandex.ru>
2024-07-21 13:09:27 +0500

    create base project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{ts,tsx}', './app/**/*.{ts,tsx}'],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    screens: {
+      mobile: { max: '767px' },
+      'after-mobile': { min: '768px' },
+      tablet: { min: '768px', max: '1439px' },
+      'before-desktop': { max: '1439px' },
+      desktop: { min: '1440px' },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
