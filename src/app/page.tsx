23d50130748268746e2a1766b4c20fdb3077a951
Kirill <qtrixnet@yandex.ru>
2024-07-21 13:29:07 +0500

    prepare Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from 'react'
 
 const Home = (): ReactElement => {
-  return <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+  return <main>Home</main>
 }
 
 export default Home
