a3ea61f101add628edc5434c51c211a5983412df
Kirill <qtrixnet@yandex.ru>
2024-07-27 16:57:48 +0500

    get bubblesort from yandex

--- src/modules/Core/alghorithms/bubbleSort.ts
+++ src/modules/Core/alghorithms/bubbleSort.ts
@@ -1,14 +1,22 @@
+const swap = (array: number[], i: number, j: number): void => {
+  if (array.length <= 1) return
+
+  const temp = array[i]
+
+  array[i] = array[j]
+  array[j] = temp
+}
+
 export const bubbleSort = (array: number[]): number[] => {
-  const { length } = array
+  let swapped = true
 
-  if (length <= 1) return array
+  while (swapped) {
+    swapped = false
 
-  for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length - 1 - i; j++) {
-      if (array[j] > array[j + 1]) {
-        const temp = array[j]
-        array[j] = array[j + 1]
-        array[j + 1] = temp
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1)
+        swapped = true
       }
     }
   }
