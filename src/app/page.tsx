ca1a0d99cf22a3b8489e60a34ab737f7f202d029
Kirill <qtrixnet@yandex.ru>
2024-07-27 15:35:58 +0500

    add titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,25 @@ const homeClassName = cx(
   'flex flex-col gap-4',
 )
 
+const titleClassName = cx(
+  // Блочная модель
+  'p-4',
+  // Оформление
+  'bg-blue-900',
+  // Типографика
+  'font-bold text-xl',
+)
+
 const TEST_ARRAY_LENGTH = 50_000
 const TEST_ARRAY = generateUnsortedArr(TEST_ARRAY_LENGTH)
 
 const Home = (): ReactElement => {
   return (
     <section className={homeClassName}>
+      <h2 className={titleClassName}>Алгоритмы сортировки</h2>
       <BubbleSort array={TEST_ARRAY} title='Сортировка пузырьком' />
       <SelectionSort array={TEST_ARRAY} title='Сортировка выбором' />
+      <h2 className={titleClassName}>Алгоритмы поиска</h2>
       <BinarySearch array={TEST_ARRAY} title='Бинарный поиск' />
     </section>
   )
