c3d46d498e8fc321fcd2de47aff477a64c54439f
Kirill <qtrixnet@yandex.ru>
2024-07-30 20:35:16 +0500

    fix data

--- src/modules/Algorithm/hocs/WithAlgorithmProps.tsx
+++ src/modules/Algorithm/hocs/WithAlgorithmProps.tsx
@@ -3,11 +3,12 @@ import { ComponentType, ReactElement } from 'react'
 import { AlgorithmComponentProps } from '@/modules/Algorithm/types/types'
 import { generateRandomNumberArray } from '@/modules/Core/utils/generateRandomNumberArray'
 import { generateReverseSortedNumberArray } from '@/modules/Core/utils/generateReverseSortedNumberArray'
+import { generateSortedNumberArray } from '@/modules/Core/utils/generateSortedNumberArray'
 
-const ARRAY_LENGTH = 10_000
+const ARRAY_LENGTH = 100_000
 const RANDOM_NUMBER_ARRAY = generateRandomNumberArray(ARRAY_LENGTH)
 const REVERSE_SORTED_NUMBER_ARRAY = generateReverseSortedNumberArray(ARRAY_LENGTH)
-const SORTED_NUMBER_ARRAY = [...REVERSE_SORTED_NUMBER_ARRAY].reverse()
+const SORTED_NUMBER_ARRAY = generateSortedNumberArray(ARRAY_LENGTH)
 
 type Props = Omit<AlgorithmComponentProps, 'randomArray' | 'reverseSortedArray' | 'sortedArray' | 'elementsCount'>
 
@@ -17,9 +18,9 @@ export const WithAlgorithmProps = (Component: ComponentType<AlgorithmComponentPr
       <Component
         {...props}
         elementsCount={ARRAY_LENGTH}
-        randomArray={RANDOM_NUMBER_ARRAY}
-        reverseSortedArray={REVERSE_SORTED_NUMBER_ARRAY}
-        sortedArray={SORTED_NUMBER_ARRAY}
+        randomArray={[...RANDOM_NUMBER_ARRAY]}
+        reverseSortedArray={[...REVERSE_SORTED_NUMBER_ARRAY]}
+        sortedArray={[...SORTED_NUMBER_ARRAY]}
       />
     )
   }
